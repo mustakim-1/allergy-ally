39fa158218a81843c9d45332bed01ec89a3584fa
mustakim-1 <meishmum@gmail.com>
2025-06-27 12:19:05 +0600

    balnk page

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { jsx as _jsx } from "react/jsx-runtime";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './my-react-app/src/App.jsx';
 const rootElement = document.getElementById('root');
 if (!rootElement) {
     throw new Error("Could not find root element to mount to");
